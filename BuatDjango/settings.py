b36f7842fffd65b8588cf94faf8a99805d9078bb
mtrq <muhammad.tarreq@ui.ac.id>
2022-09-01 20:30:39 +0700

    Django project

--- BuatDjango/settings.py
+++ BuatDjango/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-xd3rwn3@7n^nc+*#ealxc+yj*kq_ue+^h7$t$($1rzdy-u+l1s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
